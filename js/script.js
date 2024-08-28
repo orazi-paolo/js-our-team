b12778da922f27843ad44912759ffe55942670a5
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-28 15:04:21 +0200

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// creo un array con i dati forniti 
+const team = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
